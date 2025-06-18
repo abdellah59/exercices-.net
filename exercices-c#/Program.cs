1c44d9c98c14adba1ea0a50e0681b77e8b6d7d02
abdellah59 <abdellah.ait59@gmail.com>
2025-06-18 21:54:57 +0200

    add exercice 3

--- exercices-c#/Program.cs
+++ exercices-c#/Program.cs
@@ -1,48 +1,59 @@
-﻿/* See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-*/
+﻿
 
-/* exercice 1 :  Écrire un programme qui vérifie si un nombre donné est supérieur à 10.
+// exercice 1 :  Écrire un programme qui vérifie si un nombre donné est supérieur à 10.
 
 
 
-Console.WriteLine("Saisissez un nombre : ");
-int nombre = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Saisissez un nombre : ");
+//int nombre = Convert.ToInt32(Console.ReadLine());
 
 
-if (nombre < 10)
-{
-    Console.WriteLine("Le nombre est inférieur à 10");
-}
+//if (nombre < 10)
+//{
+//    Console.WriteLine("Le nombre est inférieur à 10");
+//}
 
-else
-{
-    Console.WriteLine("a est supérieur à 10");
-}
+//else
+//{
+//    Console.WriteLine("a est supérieur à 10");
+//}
 
-*/
+
 
 // exercicie 2 :  Vérifier si un nombre donné est positif, négatif ou nul.
 
 
 
-Console.WriteLine("Saisissez un nombre : ");
+//Console.WriteLine("Saisissez un nombre : ");
+//int nombre = Convert.ToInt32(Console.ReadLine());
+
+//if (nombre < 0)
+//{
+//    Console.WriteLine("Le nombre est négatif");
+//}
+//else if (nombre > 0)
+//{
+//    Console.WriteLine("Le nombre est positif");
+//}
+//else
+//{
+//    Console.WriteLine("Le nombre est nul");
+//}
+
+
+//exercice 3. Créer un programme qui affiche "Vous êtes mineur" si l’âge est inférieur à 18.
+
+Console.WriteLine("Saisissez votre age :");
 int nombre = Convert.ToInt32(Console.ReadLine());
 
-if (nombre < 0)
+if (nombre < 18)
 {
-    Console.WriteLine("Le nombre est négatif");
-}
-else if (nombre > 0)
-{
-    Console.WriteLine("Le nombre est positif");
+    Console.WriteLine("Vous êtes mineur");
 }
 else
 {
-    Console.WriteLine("Le nombre est nul");
+    Console.WriteLine("Vous êtes majeur");
 }
-
-
 
 
 
