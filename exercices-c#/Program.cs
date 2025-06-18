96bcb8ecf503489fa8557700b8b98400a4dd6844
abdellah59 <abdellah.ait59@gmail.com>
2025-06-18 16:22:34 +0200

    add exercice 1

--- exercices-c#/Program.cs
+++ exercices-c#/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/* See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+*/
+
+/* exercice 1 :  Écrire un programme qui vérifie si un nombre donné est supérieur à 10.
+
+int nombre;
+
+Console.WriteLine("Saisissez un nombre");
+Convert.ToInt32(Console.ReadLine(), out nombre);
+
+
+if (nombre < 10)
+{
+    Console.WriteLine("Le nombre est inférieur à 10");
+}
+
+else
+{
+    Console.WriteLine("a est supérieur à 10");
+}
+
+*/
+
+
+
+
+
+
+
+
